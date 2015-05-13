c1289655400f8c99ec64ac4bb7413c63a03bd1b5
Layne Gustafson <lg198@pvsd.org>
2015-05-12 21:56:53 -0400

    Initial commit.

---
+++ src/com/github/lg198/codefray/jfx/CodeFrayApplication.java
@@ -0,0 +1,16 @@
+package com.github.lg198.codefray.jfx;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class CodeFrayApplication extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+
+    }
+}
