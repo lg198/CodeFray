eaf63ba4cf8acce27848dce39d928f3478d9294d
Layne Gustafson <lg198@pvsd.org>
2015-06-01 07:22:54 -0400

    Started map generation. Filler has issues with double radius.

--- src/com/github/lg198/codefray/game/map/gen/FillerFilter.java
+++ src/com/github/lg198/codefray/game/map/gen/FillerFilter.java
@@ -1,8 +1,10 @@
 package com.github.lg198.codefray.game.map.gen;
 
 import com.github.lg198.codefray.api.math.Direction;
+import com.github.lg198.codefray.game.map.FlagTile;
 import com.github.lg198.codefray.game.map.MapTile;
 import com.github.lg198.codefray.game.map.WallTile;
+import com.github.lg198.codefray.game.map.WinTile;
 
 import java.util.Map;
 import java.util.Random;
@@ -20,7 +22,7 @@ public class FillerFilter extends GenFilter {
 
         for (int x = 0; x < map.length; x++) {
             for (int y = 0; y < map[x].length; y++) {
-                if (map[x][y] instanceof WallTile) {
+                if (map[x][y] instanceof FlagTile || map[x][y] instanceof WinTile) {
                     clearAround(map, x, y);
                 }
             }
@@ -29,9 +31,26 @@ public class FillerFilter extends GenFilter {
 
     private void clearAround(MapTile[][] map, int x, int y) {
         for (Direction d : Direction.values()) {
-            if (d.getXChange() + x >= 0 && d.getXChange() + y < map.length) {
+            //first layer
+            if (d.getXChange() + x >= 0 && d.getXChange() + x < map.length) {
                 if (d.getYChange() + y >= 0 && d.getYChange() + y < map[0].length){
                     map[x+d.getXChange()][y+d.getYChange()] = null;
+                }
+            }
+            //second layer
+            if (d.getXChange()*2 + x >= 0 && d.getXChange()*2 + x < map.length) {
+                if (d.getYChange()*2 + y >= 0 && d.getYChange()*2 + y < map[0].length){
+                    int cx = x+d.getXChange()*2, cy = y+d.getYChange()*2;
+                    map[cx][cy] = null;
+                    if (cx < x+2 && cy == y-2) { //top => right
+                        map[cx+1][cy] = null;
+                    } else if (cy < y +2 && cx == x+2) { // right => down
+                        map[cx][cy+1] = null;
+                    } else if (cx > x+2 && cy == y+2) { //bottom => left
+                        map[cx-1][cy] = null;
+                    } else if (cy > y+2 && cx == x-2) { //left => up
+                        map[cx][cy - 1] = null;
+                    }
                 }
             }
         }
