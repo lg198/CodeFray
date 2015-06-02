26739c27eeb4df029fa1596878605026546091f5
Layne Gustafson <lg198@pvsd.org>
2015-06-02 08:52:18 -0400

    Continued progress on world generation.

--- src/com/github/lg198/codefray/game/map/gen/CarveFilter.java
+++ src/com/github/lg198/codefray/game/map/gen/CarveFilter.java
@@ -1,4 +1,36 @@
 package com.github.lg198.codefray.game.map.gen;
 
-public class CarveFilter {
+import com.github.lg198.codefray.api.math.Direction;
+import com.github.lg198.codefray.api.math.Point;
+import com.github.lg198.codefray.game.map.MapTile;
+
+import java.util.Map;
+import java.util.Random;
+
+public class CarveFilter extends GenFilter {
+
+    public int width, height;
+    public Random random;
+
+
+    @Override
+    public void filter(Random r, MapTile[][] map, int width, int height, Map props) {
+        this.width = width;
+        this.height = height;
+        this.random = r;
+
+    }
+
+    public void lanuchMiner(int sx, int sy) {
+
+    }
+
+    public void mine(Point p) {
+        for (Direction d : Direction.values()) {
+            if (p.in(d).inBounds(0, width, 0, height)) {
+
+            }
+        }
+    }
+
 }
